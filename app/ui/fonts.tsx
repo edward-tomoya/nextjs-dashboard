f56ba9e93ed1c8da9264a556882a72beefc71fd2
Edward Tomoya <slayerlolz1337@gmail.com>
2024-11-20 14:09:29 +0300

    init

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ display: 'swap', subsets: ['latin'] });
+export const lusitana = Lusitana({ display: 'swap', subsets: ['latin'], weight: ['400', '700']})
