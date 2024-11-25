847ff92c30906aaa221a9ed7737e90631ccf50d6
Edward Tomoya <slayerlolz1337@gmail.com>
2024-11-25 11:45:01 +0300

    chapter 10 is done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,7 +17,7 @@ export async function fetchRevenue() {
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue>`SELECT * FROM public.revenue`;
+    const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
 
